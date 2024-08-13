143c5d5cd153788a554092bbd6abd1c1b6ce7d03
rihid <rizalhidayat006@gmail.com>
2024-08-13 22:34:54 +0700

    chore: setup drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/schema.ts" ,
+  dialect: "postgresql",
+  dbCredentials: {
+    host: 'localhost',
+    user: process.env.POSTGRES_USER as string,
+    password: process.env.POSTGRES_PASSWORD as string,
+    database: process.env.POSTGRES_DB as string,
+    port: 5445,
+  },
+});
