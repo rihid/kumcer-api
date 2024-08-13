bc1115778397e21f7f3b4c0ad706ee1601fef6c9
rihid <rizalhidayat006@gmail.com>
2024-08-13 22:35:19 +0700

    fix: modify db connection

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema"
 
-const { POSTGRES_DATABASE, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+const { POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT } = process.env;
 
-export const connection = postgres('postgres://username:password@host:port/database', {
-  host: 'localhost',
-  port: 5445,
-  database: POSTGRES_DATABASE,
-  username: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-});
+export const connection = postgres(
+  `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${
+    POSTGRES_HOST || "0.0.0.0"
+  }:${POSTGRES_PORT}/${POSTGRES_DB}`
+);
+
 export const db = drizzle(connection, { schema });
