342919841510a091099d9afb74334078bf0ce6e1
rihid <rizalhidayat006@gmail.com>
2024-08-20 21:45:50 +0700

    chore: postgres host

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   schema: "./src/schema.ts" ,
   dialect: "postgresql",
   dbCredentials: {
-    host: 'localhost',
+    host: process.env.POSTGRES_HOST as string,
     user: process.env.POSTGRES_USER as string,
     password: process.env.POSTGRES_PASSWORD as string,
     database: process.env.POSTGRES_DB as string,
