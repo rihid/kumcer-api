ec5e9b499b019f627ff6f14eb8a7b472b16844cc
rihid <rizalhidayat006@gmail.com>
2024-08-20 19:58:35 +0700

    feat: add authors & publishers schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,18 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
 
+export const authors = pgTable('authors', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+})
+export const publishers = pgTable('publishers', {
+  id: serial('id').primaryKey(),
+  publisher_name: text('publisher_name').notNull()
+})
 export const posts = pgTable('posts', {
   id: serial('id').primaryKey(),
   title: text('title').notNull(),
   body: text('body').notNull(),
-  author: text('author').notNull(),
+  author_id: integer('author_id').references(() => authors.id),
+  publisher_id: integer('publisher_id').references(() => publishers.id),
   tags: text('tags').array(),
 })
